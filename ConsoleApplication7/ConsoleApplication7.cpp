5f8cda77e858d4c785306c259cacd7c97155acd6
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-16 20:21:48 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+int main()
+{
+    double firstNumber, secondNumber;
+
+    std::cout << "Enter your first number: ";
+    std::cin >> firstNumber;
+
+    std::cout << "Enter your second number: ";
+    std::cin >> secondNumber;
+
+    std::cout << "Menu:\n";
+    std::cout << "0 - exit\n";
+    std::cout << "1 - addition\n";
+    std::cout << "2 - subtraction\n";
+    std::cout << "3 - multiplication\n";
+    std::cout << "4 - division\n";
+    std::cout << "5 - remainder\n";
+    std::cout << "Make a choice!\n";
+
+    int operation;
+    std::cin >> operation;
+
+    if (operation > 5)                  
+        std::cout << "There is no operation for the entered value";
+
+    if (operation == 0)
+        return 0;
+
+    if (operation == 1)
+        std::cout << "\nAnswer: " << firstNumber + secondNumber;
+
+    if (operation == 2)
+        std::cout << "\nAnswer: " << firstNumber - secondNumber;
+
+    if (operation == 3)
+        std::cout << "\nAnswer: " << firstNumber * secondNumber;
+
+    if (operation == 4)
+    {
+        if (secondNumber == 0)
+            std::cout << "Error.\nIt is not possible to divide by zero";
+        else
+            std::cout << "\nAnswer: " << firstNumber / secondNumber;
+    }
+
+    if (operation == 5)
+    {
+        if (secondNumber == 0)
+            std::cout << "Error.\nIt is not possible to divide by zero";
+        else
+            std::cout << "\nAnswer: " << int(firstNumber) % int(secondNumber);
+    }
+}
